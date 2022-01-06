760e3947e94811d65a41390983a05d409da5dc62
Thomas Petchel <tpetchel@gmail.com>
2022-01-06 12:16:04 -0800

    F#

---
+++ fsharp/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let (|Fizz|_|) n =
+    if n % 3 = 0 then Some("Fizz")
+    else None
+
+let (|Buzz|_|) n =
+    if n % 5 = 0 then Some("Buzz")
+    else None
+
+let r = { 1 .. 100 } |> Seq.map (fun i -> 
+    match i with 
+    | Fizz(s) | Buzz(s) -> s
+    | _ -> i.ToString()) |> Seq.toArray
+    
+printfn $"{String.Join(',', r)}"
